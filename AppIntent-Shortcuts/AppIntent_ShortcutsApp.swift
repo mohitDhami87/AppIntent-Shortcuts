04d5fbda4f7f05e881ab09b9944abf3aaa214cb9
Mohit Sharma <mohitksharmaa@yahoo.com>
2025-04-10 15:31:59 +0530

    Initial Commit

---
+++ AppIntent-Shortcuts/AppIntent_ShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntent_ShortcutsApp.swift
+//  AppIntent-Shortcuts
+//
+//  Created by Mohit Sharma on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AppIntent_ShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntent-Shortcuts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIntent-Shortcuts
+//
+//  Created by Mohit Sharma on 10/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
